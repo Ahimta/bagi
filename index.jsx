d4764f0e6a03e008bbf5530de06374440d888d54
Abdullah Alansari <ahimta@gmail.com>
2016-09-03 02:00:36 +0300

    Update eid date

--- index.jsx
+++ index.jsx
@@ -211,7 +211,7 @@ var Remainders = React.createClass({
 })
 
 var remainders = [
-  {title: 'عيد الأضحية', date: new Date(2016, 8, 11, 0).getTime(), style: 'success'},
+  {title: 'عيد الأضحية', date: new Date(2016, 8, 12, 0).getTime(), style: 'success'},
   {title: 'بداية الدراسة', date: new Date(2016, 8, 18, 0).getTime(), style: 'danger'},
   {title: 'FIFA 17', date: new Date(2016, 8, 27, 0).getTime(), style: 'success'},
   {title: 'بداية إجازة منتصف الفصل الأول', date: new Date(2016, 10, 10, 0).getTime(), style: 'success'},
