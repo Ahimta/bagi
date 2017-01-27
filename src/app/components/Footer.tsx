28a53e6419dc114b55b41616d8f06e92b537aa9f
Abdullah Alansari <ahimta@gmail.com>
2017-01-27 04:32:47 +0300

    Add PWA support

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,10 +5,17 @@ interface IState { }
 
 export default class Footer extends React.Component<IProps, IState> {
   render() {
-    return (<footer className='lead text-center'>
-      <p>
-        <a dir='rtl' href='https://linkedin.com/in/ahimta' target='_blank'>
-          &copy; 2017&nbsp;
+    return (<footer>
+      <p className='text-center' dir='rtl'>
+        بعض الأيقونات المستخدمة من تصميم&nbsp;
+          <a href='http://www.flaticon.com/authors/roundicons' target='_blank' rel='noopener'>Roundicons</a>&nbsp;
+          تحت رخصة&nbsp;
+          <a href='CC 3.0 BY' target='_blank' rel='noopener'>CC 3.0 BY</a>
+      </p>
+      <p className='text-center' dir='rtl'>
+        <a href='https://sa.linkedin.com/in/ahimta' target='_blank'>
+          &copy;
+          2017
           عبدالله الأنصاري
         </a>
       </p>
