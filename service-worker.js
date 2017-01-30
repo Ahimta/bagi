ed26bb384c55e252c0d325e91b325455252a3059
Abdullah Alansari <ahimta@gmail.com>
2017-01-30 22:06:56 +0300

    Update 2017-01-30T19:06:38.708Z

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["app-382aca103a230d8b4b89.js","121594f6b95ffc07e616a3e1a00d08e3"],["app/images/icon-128x128.png","51b2af0b059748d66448dbfacd6e1fb2"],["app/images/icon-144x144.png","effc03c9e8217a1eb189e871c9d38dfe"],["app/images/icon-152x152.png","5531c74b13b54fd6fefb2d81c63d33d7"],["app/images/icon-192x192.png","a316b15fc2c4062e5a8d8c87f27d826f"],["app/images/icon-256x256.png","bdcc2f78be9308564996ead4e2513cae"],["app/vendor/bootstrap-v3.3.7.min.css","ec3bb52a00e176a7181d454dffaea219"],["index.html","7d3edfe10fe8d5af9756141f3d4fa3c2"],["index.spec.js","4b46c335c62c3125e9cbdf63a49560fa"],["vendor-382aca103a230d8b4b89.js","8ff03220f2ba45ad56437550e7e56e72"]];
+var precacheConfig = [["app-820f54af1287e2dda32c.js","a7e8fa8d4137b97221b3ce2032557a50"],["app/images/icon-128x128.png","51b2af0b059748d66448dbfacd6e1fb2"],["app/images/icon-144x144.png","effc03c9e8217a1eb189e871c9d38dfe"],["app/images/icon-152x152.png","5531c74b13b54fd6fefb2d81c63d33d7"],["app/images/icon-192x192.png","a316b15fc2c4062e5a8d8c87f27d826f"],["app/images/icon-256x256.png","bdcc2f78be9308564996ead4e2513cae"],["app/sw-push-notifications.js","a3e64b4702116357370cfada432a3bd6"],["app/vendor/bootstrap-v3.3.7.min.css","ec3bb52a00e176a7181d454dffaea219"],["index.html","c02ac4267b8e88a6ac09c3423939a799"],["index.spec.js","4b46c335c62c3125e9cbdf63a49560fa"],["vendor-820f54af1287e2dda32c.js","fd5258718d9a87752bfa3cdc1481b350"]];
 var cacheName = 'sw-precache-v2--' + (self.registration ? self.registration.scope : '');
 
 
@@ -256,4 +256,6 @@ toolbox.router.get(/https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.7\//, tool
 
 
 
+
+importScripts("app/sw-push-notifications.js");
 
