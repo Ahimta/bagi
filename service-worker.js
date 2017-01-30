3dc2e1306e6fcf118b59faadbd805eddda541154
Abdullah Alansari <ahimta@gmail.com>
2017-01-30 04:23:17 +0300

    Update 2017-01-30T01:23:10.289Z

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["app-6e2e771020a3806a3510.js","cee82d459fbfa625f500eefdae14a9c3"],["app/images/icon-128x128.png","51b2af0b059748d66448dbfacd6e1fb2"],["app/images/icon-144x144.png","effc03c9e8217a1eb189e871c9d38dfe"],["app/images/icon-152x152.png","5531c74b13b54fd6fefb2d81c63d33d7"],["app/images/icon-192x192.png","a316b15fc2c4062e5a8d8c87f27d826f"],["app/images/icon-256x256.png","bdcc2f78be9308564996ead4e2513cae"],["app/vendor/bootstrap-v3.3.7.min.css","ec3bb52a00e176a7181d454dffaea219"],["index.html","344fdd4f8ff1f4995686a23f6c80804b"],["index.spec.js","4b46c335c62c3125e9cbdf63a49560fa"],["vendor-6e2e771020a3806a3510.js","1e0d928a1a2e119b06f6055bd2999bee"]];
+var precacheConfig = [["app-247c4823867f733cf1ce.js","ae05ca56a34607c941ec1168905c4f0b"],["app/images/icon-128x128.png","51b2af0b059748d66448dbfacd6e1fb2"],["app/images/icon-144x144.png","effc03c9e8217a1eb189e871c9d38dfe"],["app/images/icon-152x152.png","5531c74b13b54fd6fefb2d81c63d33d7"],["app/images/icon-192x192.png","a316b15fc2c4062e5a8d8c87f27d826f"],["app/images/icon-256x256.png","bdcc2f78be9308564996ead4e2513cae"],["app/vendor/bootstrap-v3.3.7.min.css","ec3bb52a00e176a7181d454dffaea219"],["index.html","0c8ad896878fe08470f7f1665474e832"],["index.spec.js","4b46c335c62c3125e9cbdf63a49560fa"],["vendor-247c4823867f733cf1ce.js","e5e2a5ef5ada2534ae5a8c05c349454a"]];
 var cacheName = 'sw-precache-v2--' + (self.registration ? self.registration.scope : '');
 
 
