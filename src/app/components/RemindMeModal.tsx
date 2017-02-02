0ff86cdc60eb4bf93e3b1e01fe79b102d77decd0
Abdullah Alansari <ahimta@gmail.com>
2017-02-02 19:27:03 +0300

    Better reminder dialog UI

--- src/app/components/RemindMeModal.tsx
+++ src/app/components/RemindMeModal.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
-import { Button, DropdownButton, FormControl, FormGroup, InputGroup, MenuItem, Modal } from 'react-bootstrap'
+import {
+  Alert, Button, DropdownButton, FormControl, FormGroup, InputGroup, MenuItem, Modal, ProgressBar
+} from 'react-bootstrap'
 
 import * as notifications from '../notifications'
 import IBagiEvent from '../types/IBagiEvent'
@@ -7,6 +9,7 @@ import TimeUnit from '../types/TimeUnit'
 import t from '../translate'
 
 const initialState: IState = {
+  loading: false,
   timeUnit: 'minute',
   timeValue: 5
 }
@@ -20,6 +23,7 @@ interface IProps {
 }
 
 interface IState {
+  readonly loading: boolean;
   readonly timeUnit: TimeUnit;
   readonly timeValue: number;
 }
@@ -33,34 +37,38 @@ export default class RemindMeModal extends React.Component<IProps, IState>
 
   render() {
     const {event, hideModal, showModal} = this.props
-    const {timeUnit, timeValue} = this.state
+    const {loading, timeUnit, timeValue} = this.state
+
+    const ModalBody = loading ? <ProgressBar now={100} striped /> : (<form>
+      <Alert bsSize='xs' bsStyle='warning' dir='rtl'>ﻷسباب تقنية، ميزة التذكير تحتاج اتصال إنترنت!</Alert>
+
+      <FormGroup>
+        <InputGroup>
+          <DropdownButton componentClass={InputGroup.Button} id={`reminder-${event.date.getTime()}`}
+            title={t(timeUnit)} value={timeUnit} onSelect={this.handleChangeFactory('timeUnit')} pullRight>
+            <MenuItem active={timeUnit === 'second'} className='text-right' eventKey='second'>{t('second')}</MenuItem>
+            <MenuItem active={timeUnit === 'minute'} className='text-right' eventKey='minute'>{t('minute')}</MenuItem>
+            <MenuItem active={timeUnit === 'hour'} className='text-right' eventKey='hour'>{t('hour')}</MenuItem>
+            <MenuItem active={timeUnit === 'day'} className='text-right' eventKey='day'>{t('day')}</MenuItem>
+            <MenuItem active={timeUnit === 'week'} className='text-right' eventKey='week'>{t('week')}</MenuItem>
+            <MenuItem active={timeUnit === 'month'} className='text-right' eventKey='month'>{t('month')}</MenuItem>
+          </DropdownButton>
+          <FormControl dir='rtl' min={0} max={1000000} type='number' value={timeValue}
+            onChange={(e) => this.setState({ timeValue: (e.target as any).value } as IState)} required />
+          <InputGroup.Addon>قبل</InputGroup.Addon>
+        </InputGroup>
+      </FormGroup>
+    </form>)
 
     return (<Modal show={showModal} onEnter={() => this.setState(initialState)} onHide={hideModal}>
       <Modal.Header className='text-center' closeButton>
         <Modal.Title>إضافة تذكير</Modal.Title>
       </Modal.Header>
 
-      <Modal.Body>
-        <FormGroup>
-          <InputGroup>
-            <DropdownButton componentClass={InputGroup.Button} id={`reminder-${event.date.getTime()}`}
-              title={t(timeUnit)} value={timeUnit} onSelect={this.handleChangeFactory('timeUnit')} pullRight>
-              <MenuItem active={timeUnit === 'second'} className='text-right' eventKey='second'>{t('second')}</MenuItem>
-              <MenuItem active={timeUnit === 'minute'} className='text-right' eventKey='minute'>{t('minute')}</MenuItem>
-              <MenuItem active={timeUnit === 'hour'} className='text-right' eventKey='hour'>{t('hour')}</MenuItem>
-              <MenuItem active={timeUnit === 'day'} className='text-right' eventKey='day'>{t('day')}</MenuItem>
-              <MenuItem active={timeUnit === 'week'} className='text-right' eventKey='week'>{t('week')}</MenuItem>
-              <MenuItem active={timeUnit === 'month'} className='text-right' eventKey='month'>{t('month')}</MenuItem>
-            </DropdownButton>
-            <FormControl dir='rtl' min={0} max={1000000} type='number' value={timeValue}
-              onChange={(e) => this.setState({ timeValue: (e.target as any).value } as IState)} />
-            <InputGroup.Addon>قبل</InputGroup.Addon>
-          </InputGroup>
-        </FormGroup>
-      </Modal.Body>
+      <Modal.Body>{ModalBody}</Modal.Body>
 
       <Modal.Footer>
-        <Button bsStyle='success' onClick={this.remindMe} block>ذكرني</Button>
+        <Button active={loading} bsStyle='success' onClick={this.remindMe} block>ذكرني</Button>
       </Modal.Footer>
     </Modal>)
   }
@@ -71,9 +79,12 @@ export default class RemindMeModal extends React.Component<IProps, IState>
     const {event, hideModal} = this.props
     const {timeUnit, timeValue} = this.state
 
+    this.setState({ loading: true } as IState)
     notifications.scheduleNotification(event, timeUnit, timeValue).then(() => {
+      this.setState({ loading: false } as IState)
       hideModal()
     }).catch(err => {
+      this.setState({ loading: false } as IState)
       console.log('Notification failed with: ', err)
     })
   }
