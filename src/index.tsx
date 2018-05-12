81bc94431768b2e254d8ff541503b8b13515e8dc
Abdullah Alansari <ahimta@gmail.com>
2018-05-12 20:11:06 +0300

    Add client error-reporting using Sentry:smiley:

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import * as Raven from 'raven-js'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
@@ -5,8 +7,14 @@ import * as injectTapEventPlugin from 'react-tap-event-plugin'
 
 import App from './app/containers/App'
 
-// needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin()
+Raven
+  .config('https://7b2db5e403a44579bca792256504713b@sentry.io/1205564')
+  .install()
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'))
+Raven.context(() => {
+  // needed for onTouchTap
+  // http://stackoverflow.com/a/34015469/988941
+  injectTapEventPlugin()
+
+  ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'))
+})
